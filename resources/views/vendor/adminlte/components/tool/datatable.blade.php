53e4ac2fc469a88c225752593f438dbe69ff9bc1
Renildo Paes <renildopaesjoob@gmail.com>
2022-07-01 17:51:00 -0300

    table-alunos

--- resources/views/vendor/adminlte/components/tool/datatable.blade.php
+++ resources/views/vendor/adminlte/components/tool/datatable.blade.php
@@ -2,49 +2,47 @@
 
 <div class="table-responsive">
 
-<table id="{{ $id }}" style="width:100%" {{ $attributes->merge(['class' => $makeTableClass()]) }}>
-
-    {{-- Table head --}}
-    <thead @isset($headTheme) class="thead-{{ $headTheme }}" @endisset>
-        <tr>
-            @foreach($heads as $th)
-                <th @isset($th['width']) style="width:{{ $th['width'] }}%" @endisset
-                    @isset($th['no-export']) dt-no-export @endisset>
-                    {{ is_array($th) ? ($th['label'] ?? '') : $th }}
-                </th>
-            @endforeach
-        </tr>
-    </thead>
-
-    {{-- Table body --}}
-    <tbody>{{ $slot }}</tbody>
-
-    {{-- Table footer --}}
-    @isset($withFooter)
-        <tfoot @isset($footerTheme) class="thead-{{ $footerTheme }}" @endisset>
+    <table id="{{ $id }}" style="{{ 'border-radius: 10px 10px 0px 0px' }}" class="{{ 'table table-striped table-hover table-inverse table-responsive' }}">
+        {{-- Table head --}}
+        <thead class="{{ 'table-dark' }}">
             <tr>
                 @foreach($heads as $th)
-                    <th>{{ is_array($th) ? ($th['label'] ?? '') : $th }}</th>
+                    <th  class="col-md-3">
+                        {{ is_array($th) ? ($th['label'] ?? '') : $th }}
+                    </th>
                 @endforeach
             </tr>
-        </tfoot>
-    @endisset
+        </thead>
 
-</table>
+        {{-- Table body --}}
+        <tbody>{{ $slot }}</tbody>
+
+        {{-- Table footer --}}
+        @isset($withFooter)
+            <tfoot @isset($footerTheme) class="thead-{{ $footerTheme }}" @endisset>
+                <tr>
+                    @foreach($heads as $th)
+                        <th>{{ is_array($th) ? ($th['label'] ?? '') : $th }}</th>
+                    @endforeach
+                </tr>
+            </tfoot>
+        @endisset
+
+    </table>
 
 </div>
 
 {{-- Add plugin initialization and configuration code --}}
 
-@push('js')
-<script>
+{{-- @push('js')
+    <script>
 
-    $(() => {
-        $('#{{ $id }}').DataTable( @json($config) );
-    })
+        $(() => {
+            $('#{{ $id }}').DataTable( @json($config) );
+        })
 
-</script>
-@endpush
+    </script>
+@endpush --}}
 
 {{-- Add CSS styling --}}
 
