a798c682b6f6131cb33f01204d91165ce017c8ef
RenildoPaesJob <renildopaesjoob@gmail.com>
2024-04-25 10:59:12 -0300

    vite

---
+++ public/build/assets/LogoShieldTeam-94510556.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-f0dd2ff0.js";const e="/build/assets/logo_shield_team_bjj_color-859bdaa8.svg";function a({className:o},t){return s.jsx("img",{className:o,src:e,alt:"Meu SVG"})}export{a as L};
